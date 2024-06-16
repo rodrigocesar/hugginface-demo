f86377b394a8b00170645fdea9ffc984e20743e5
Rodrigo Cesar da Silva <rodrigocesar.jo@gmail.com>
2024-06-16 15:55:42 +0100

    adding scaffold

---
+++ app.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+import gradio as gr
+
+model = pipeline("summarization")
+
+def summarize_text(text):
+    return model(text)[0]['summary_text']
+
+with gr.Blocks() as demo:
+    text = gr.Textbox(lines=10, label="Input Text")
+    summary = gr.Textbox(lines=10, label="Summary")
+    gr.Interface(fn=summarize_text, inputs=text, outputs=summary).launch()
+    
+demo.launch()
